55811381f6d318b13b62d4e6332b98d307ed8883
c0a22141 <c0a22141f9@edu.teu.ac.jp>
2023-11-28 14:38:39 +0900

    練習3

--- dodge_bomb.py
+++ dodge_bomb.py
@@ -5,13 +5,20 @@ import random
 WIDTH, HEIGHT = 1400, 800
 
 
-
+delta = {
+    pg.K_UP: (0, -5),
+    pg.K_DOWN: (0, +5),
+    pg.K_RIGHT: (+5, 0),
+    pg.K_LEFT: (-5, 0)
+}
 def main():
     pg.display.set_caption("逃げろ！こうかとん")
     screen = pg.display.set_mode((WIDTH, HEIGHT))
     bg_img = pg.image.load("ex02/fig/pg_bg.jpg")
     kk_img = pg.image.load("ex02/fig/3.png")
     kk_img = pg.transform.rotozoom(kk_img, 0, 2.0)
+    kk_rect = kk_img.get_rect()
+    kk_rect.center= 900,400
     clock = pg.time.Clock()
     tmr = 0
     bomb_img=pg.Surface((20,20))
@@ -27,9 +34,17 @@ def main():
         for event in pg.event.get():
             if event.type == pg.QUIT: 
                 return
+            
+        key_lst = pg.key.get_pressed()
+        sum_mv =[0, 0]
+        for k, tpl in delta.items():
+            if key_lst[k]:
+                sum_mv[0] += tpl[0]
+                sum_mv[1] += tpl[1]
 
         screen.blit(bg_img, [0, 0])
-        screen.blit(kk_img, [900, 400])
+        kk_rect.move_ip(sum_mv[0], sum_mv[1])
+        screen.blit(kk_img, kk_rect)
         bomb_rect.move_ip(vx, vy)
         screen.blit(bomb_img, bomb_rect)
         pg.display.update()
